96a47af05e9af321733970366bcae0412ba63392
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-12-16 11:52:08 -0600

    displaying winter or summer message and styling, loading spinner

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, errorMessage: '' };
-
+  state = { lat: null, errorMessage: '' };
+  
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    return (
-      <div>
-        Latitude: { this.state.lat }
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: { this.state.errorMessage }</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+
+    return <Spinner message="Please accept location request" />;
   }
 }
 
